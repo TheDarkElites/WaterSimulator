5b3ab4703d0267bff570948eab1c643531ddc5e4
TheDarkElites <thedarkredstone@gmail.com>
2026-03-31 12:58:32 -0400

    Window init and callback function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
-#include
+#include <GL/glut.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+static int WIDTH = 1920;
+static int HEIGHT = 1080;
+
+static void RenderSceneCB() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv) {
+
+    //Init
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(WIDTH, HEIGHT);
+    glutInitWindowPosition(0, 0);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    //Window
+    int windID = glutCreateWindow("WaterSimulator");
+
+    glutDisplayFunc(RenderSceneCB);
+
+    glutMainLoop();
+
     return 0;
 }
