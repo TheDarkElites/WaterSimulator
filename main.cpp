e609a2352f095809ec8fa5bb57fd0e53ede67713
tigritik <tigritik@gmail.com>
2026-03-31 19:12:21 -0400

    basic arg parsing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <GL/glut.h>
 
+using std::cout;
+using std::endl;
+using std::stoi;
+
 static int WIDTH = 1920;
 static int HEIGHT = 1080;
 
@@ -10,6 +14,34 @@ static void RenderSceneCB() {
 }
 
 int main(int argc, char** argv) {
+    int gridX, gridY;
+    bool randomInit = true;
+
+    if (argc == 1) {
+        gridX = 100;
+        gridY = 100;
+    }
+    else if (argc >= 3) {
+        gridX = stoi(argv[1]);
+        gridY = stoi(argv[2]);
+        // only random init if filename not provided
+        randomInit = argc == 3;
+    }
+    else {
+        cout << "Usage: " << argv[0] << " [cellsX] [cellsY] [initial-steup-file]" << endl;
+    }
+
+    if (randomInit) {
+        cout << "Randomly initializing " << gridX << " by " << gridY << " grid" << endl;
+        cout << "Operation currently not supported!" << endl;
+        exit(EXIT_SUCCESS);
+    }
+    else {
+        const char* filename = argv[3];
+        cout << "Initializing " << gridX << " by " << gridY << " grid from " << filename << endl;
+        cout << "Operation currently not supported!" << endl;
+        exit(EXIT_SUCCESS);
+    }
 
     //Init
     glutInit(&argc, argv);
