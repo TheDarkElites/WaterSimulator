61823da10dacd06f00e1d06a5a52597b7be4fa90
TheDarkElites <thedarkredstone@gmail.com>
2026-03-31 22:16:33 -0400

    added initial shader support

--- main.cpp
+++ main.cpp
@@ -6,31 +6,124 @@
 static int WIDTH = 1920;
 static int HEIGHT = 1080;
 
-const char* pVSFileName = "shader.vs";
-const char* pFSFileName = "shader.fs";
+const char* pVSFileName = "../shader/shader.vs";
+const char* pFSFileName = "../shader/shader.fs";
 
-static void RenderSceneCB() {
+GLuint VBO;
+
+static void RenderSceneCB()
+{
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // Position Attribute (Location 0)
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+
+    // UV Attribute (Location 1)
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glDisableVertexAttribArray(0);
+
     glutSwapBuffers();
 }
 
 static void CreateVertexBuffer() {
+    float vertices[] = {
+        // Positions          // UVs
+        -1.0f,  1.0f, 0.0f,   0.0f, 1.0f, // Top Left
+        -1.0f, -1.0f, 0.0f,   0.0f, 0.0f, // Bottom Left
+         1.0f, -1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
 
+        -1.0f,  1.0f, 0.0f,   0.0f, 1.0f, // Top Left
+         1.0f, -1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
+         1.0f,  1.0f, 0.0f,   1.0f, 1.0f  // Top Right
+    };
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+{
+    GLuint ShaderObj = glCreateShader(ShaderType);
+
+    if (ShaderObj == 0) {
+        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+        exit(0);
+    }
+
+    const GLchar* p[1];
+    p[0] = pShaderText;
+
+    GLint Lengths[1];
+    Lengths[0] = (GLint)strlen(pShaderText);
+
+    glShaderSource(ShaderObj, 1, p, Lengths);
+
+    glCompileShader(ShaderObj);
+
+    GLint success;
+    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+        exit(1);
+    }
+
+    glAttachShader(ShaderProgram, ShaderObj);
 }
 
 static void CompileShaders() {
-    GLuint ShaderProgram = __glewCreateProgram();
+    GLuint ShaderProgram = glCreateProgram();
 
     if (ShaderProgram == 0) {
         fprintf(stderr, "Error creating shader program\n");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
     std::string vs, fs;
 
     if (!ReadFile(pVSFileName, vs)) {
-        exit(EXIT_FAILURE);
+        exit(1);
+    };
+
+    AddShader(ShaderProgram, vs.c_str(), GL_VERTEX_SHADER);
+
+    if (!ReadFile(pFSFileName, fs)) {
+        exit(1);
+    };
+
+    AddShader(ShaderProgram, fs.c_str(), GL_FRAGMENT_SHADER);
+
+    GLint Success = 0;
+    GLchar ErrorLog[1024] = { 0 };
+
+    glLinkProgram(ShaderProgram);
+
+    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+    if (Success == 0) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        exit(1);
     }
+
+    glValidateProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+    if (!Success) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glUseProgram(ShaderProgram);
 }
 
 int main(int argc, char** argv) {
@@ -42,15 +135,18 @@ int main(int argc, char** argv) {
     glutInitWindowPosition(0, 0);
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-    //Glew
-    GLenum res = glewInit();
-    if (res != GLEW_OK) {
-        fprintf(stderr, "Glew Error: '%s'\n", glewGetErrorString(res));
-        return EXIT_FAILURE;
-    }
-
     //Window
     int windID = glutCreateWindow("WaterSimulator");
+
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    CreateVertexBuffer();
+
+    CompileShaders();
 
     glutDisplayFunc(RenderSceneCB);
 
