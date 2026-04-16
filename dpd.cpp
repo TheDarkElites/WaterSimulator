722b530b8a2965810a86d629c218d120604ccff2
tigritik <59628636+tigritik@users.noreply.github.com>
2026-04-15 20:53:44 -0400

    dissipative force

--- dpd.cpp
+++ dpd.cpp
@@ -18,9 +18,19 @@ vector compute_force_c(particle& i, particle& j) {
 }
 
 /* Dissipative Force */
+float weight_d(float r) {
+  int rc = 10; // cutoff radius 10m (could change)
+  return (1- (r/rc)) * (1- (r/rc));
+}
+
 vector compute_force_d(particle& i, particle& j) {
-  /* TODO */
-  return vector(0.0, 0.0, 0.0);
+  float gamma = 4.5; // hard coded for water
+  vector r_ij = j.pos - i.pos;
+  vector v_ij = j.vel - i.vel;
+  float r = std::sqrt(r_ij * r_ij);
+  float w_D = weight_d(r);
+  vector r_hat = (1/r) * r_ij; // same as unit vec e
+  return -gamma * w_D * (v_ij * r_hat) * r_hat;
 }
 
 /* Random Force */
